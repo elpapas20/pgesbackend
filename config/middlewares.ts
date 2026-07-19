7caa321b015670ec81b36908fd849124cf469baf
elpapas20 <elpapas20@users.noreply.github.com>
2026-07-18 22:47:14 -0500

    Chore: agregar nueva URL de Vercel

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -13,6 +13,7 @@ export default [
         'http://localhost:3000', // Tu entorno local
         'http://localhost:1337', // El admin de Strapi local
         'https://ihcbackend.onrender.com', // Tu backend
+        'https://frond-l0g62eti6-2021204089danyelo-7175s-projects.vercel.app', // Última URL de Vercel (frond)
         'https://frond-jtzvn9m5k-2021204089danyelo-7175s-projects.vercel.app', // Última URL de Vercel
         'https://frond-fy0hok5qm-2021204089danyelo-7175s-projects.vercel.app', // Nueva URL de Vercel (frond)
         'https://pgesfrond-git-vercel-re-07a23d-2021204089danyelo-7175s-projects.vercel.app' // ¡AÑADE TU URL DE VERCEL AQUÍ!
